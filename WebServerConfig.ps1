c4d551f023ee00a02edb0b9cd57d4740076114c2
adv-dkluin <52167880+adv-dkluin@users.noreply.github.com>
2019-07-17 14:45:11 +0200

    Update WebServerConfig.ps1

--- WebServerConfig.ps1
+++ WebServerConfig.ps1
@@ -1,9 +1,5 @@
 Configuration WebServerConfig
 {
-
-    # Import the module that contains the resources we're using.
-    Import-DscResource -ModuleName PsDesiredStateConfiguration
-
     # The Node statement specifies which targets this configuration will be applied to.
     Node ("localhost") {
 
